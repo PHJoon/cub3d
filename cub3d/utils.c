f08ed80d83a0b4ce57290dea24cfbe8ba19b60f8
PHJoon <phj9503@gmail.com>
2023-08-22 15:38:07 +0900

    주석 제거 및 map.txt 삭제

--- cub3d/utils.c
+++ cub3d/utils.c
@@ -6,7 +6,7 @@
 /*   By: hyungjpa <hyungjpa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 12:28:13 by hyungjpa          #+#    #+#             */
-/*   Updated: 2023/08/22 13:34:42 by hyungjpa         ###   ########.fr       */
+/*   Updated: 2023/08/22 15:34:16 by hyungjpa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,3 @@ t_flag	flag_check(int flag)
 		return (FAIL);
 	return (SUCCESS);
 }
-
-// void	display(t_info *info)
-// {
-//     printf("dir: %s, %s, %s, %s\n", info->dir[0], info->dir[1], info->dir[2], info->dir[3]);
-//     printf("fc : %s, %s, %s, %s, %s, %s\n", info->fc[0], info->fc[1], info->fc[2], info->fc[3], info->fc[4], info->fc[5]);
-//     // printf("map_before : %s\n", info->map_before);
-//     printf("player_x: %d player_y: %d\n", info->player_x, info->player_y);
-//     printf("player_dir: %c\n", info->player_dir);
-//     int t = -1;
-//     int k;
-
-//     printf("map : \n");
-//     while (++t < info->height)
-//     {
-//         k = -1;
-//         while (++k < info->width)
-//             printf("%c", info->map[t][k]);
-//         printf("\n");
-//     }
-//     t = -1;
-
-//     printf("map->test : \n");
-//     while (++t < info->height)
-//     {
-//         k = -1;
-//         while (++k < info->width)
-//             printf("%c", info->map_test[t][k]);
-//         printf("\n");
-//     }
-// }
